797ca732d05a91c9c53f079498d9f3d5fd2ee112
henrypp <support@henrypp.org>
2023-02-03 21:46:27 +0600

    ++version

---
+++ src/app.h
@@ -0,0 +1,11 @@
+// Drive Dot Shield
+// Copyright (c) 2012-2023 Henry++
+
+#pragma once
+
+#define APP_NAME L"Drive Dot Shield"
+#define APP_NAME_SHORT L"drivedotshield"
+#define APP_VERSION L"2.0.1"
+#define APP_VERSION_RES 2,0,1,0
+#define APP_AUTHOR L"Henry++"
+#define APP_COPYRIGHT L"(c) 2012-2023 " APP_AUTHOR L". All Rights Reversed."
