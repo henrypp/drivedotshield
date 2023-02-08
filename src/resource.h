2553d21b3e98d66828587278d684ff608a843f52
henrypp <support@henrypp.org>
2023-02-08 18:58:50 +0600

    upd resources

--- src/resource.h
+++ src/resource.h
@@ -9,42 +9,49 @@
 #define IDA_MAIN 1
 
 // Menus
-#define IDM_MAIN		                        100
-#define IDM_DRIVES_MENU							101
-#define IDM_PROPERTIES_MENU	                    102
+#define IDM_MAIN 100
+#define IDM_DRIVES 101
+#define IDM_PROPERTIES 102
 
 // Dialogs
-#define IDD_MAIN	                            100
+#define IDD_MAIN 100
+#define IDD_PROPERTIES 101
 
 // Main Dlg
-#define IDC_DRIVES		                        100
-#define IDC_STATUSBAR                           101
+#define IDC_DRIVES 100
+#define IDC_STATUSBAR 101
+
+// Properties Dlg
+#define IDC_PROPERTIES 100
+#define IDC_OK 101
 
 // Main Menu
-#define IDM_SETTINGS                            40000
-#define IDM_EXIT                                40001
-#define IDM_WEBSITE                             40003
-#define IDM_CHECK_UPDATES		                40004
-#define IDM_ABOUT                               40005
+#define IDM_EXIT 40000
+#define IDM_SETTINGS 40001
+#define IDM_ALWAYSONTOP_CHK 40002
+#define IDM_CHECKUPDATES_CHK 40003
+#define IDM_WEBSITE 40004
+#define IDM_CHECK_UPDATES 40005
+#define IDM_ABOUT 40006
 
 // Drives Menu
-#define IDM_OPEN                                1000
-#define IDM_PROTECT                             1001
-#define IDM_UNPROTECT                          	1002
-#define IDM_REFRESH                             1003
-#define IDM_LOCK                           		1004
-#define IDM_UNLOCK                            	1005
-#define IDM_UNLOCK_ALL                         	1006
-#define IDM_EJECT                               1007
-#define IDM_FORMAT                              1008
-#define IDM_PROPERTIES                          1009
-#define IDM_COPY								1010
+#define IDM_OPEN 1000
+#define IDM_PROTECT 1001
+#define IDM_UNPROTECT 1002
+#define IDM_REFRESH 1003
+#define IDM_LOCK 1004
+#define IDM_UNLOCK 1005
+#define IDM_UNLOCK_ALL 1006
+#define IDM_EJECT 1007
+#define IDM_FORMAT 1008
+#define IDM_PROPERTY 1009
+#define IDM_COPY 1010
 
 // Icons
-#define IDI_MAIN	                            100
-#define IDI_NORMAL								101
-#define IDI_PROTECTED							102
-#define IDI_INFECTED							103
-#define IDI_LOCKED								104
+#define IDI_MAIN 100
+#define IDI_NORMAL 101
+#define IDI_PROTECTED 102
+#define IDI_INFECTED 103
+#define IDI_LOCKED 104
 
 #endif
