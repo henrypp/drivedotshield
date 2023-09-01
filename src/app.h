24a1cc127767e01fbd933ba94d783df58146d8b8
henrypp <support@henrypp.org>
2023-09-01 15:09:13 +0600

    ++version

--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"Drive Dot Shield"
 #define APP_NAME_SHORT L"drivedotshield"
-#define APP_VERSION L"2.0.1"
-#define APP_VERSION_RES 2,0,1,0
+#define APP_VERSION L"2.1"
+#define APP_VERSION_RES 2,1,0,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2012-2023 " APP_AUTHOR L". All Rights Reversed."
