9a352c0e15bd2226d702a9ac6b717222914e73ce
henrypp <support@henrypp.org>
2023-09-01 15:05:45 +0600

    cleanup

--- src/main.h
+++ src/main.h
@@ -11,13 +11,13 @@
 
 typedef struct _DRIVE_LOCK
 {
+	PR_STRING label;
+	PR_STRING file_system;
+
+	LARGE_INTEGER total_space;
+	LARGE_INTEGER free_space;
+
 	HANDLE hdrive;
-
-	WCHAR label[MAX_PATH];
-	WCHAR file_system[MAX_PATH];
-
-	ULARGE_INTEGER total_space;
-	ULARGE_INTEGER free_space;
 
 	ULONG serial_number;
 } DRIVE_LOCK;
@@ -29,54 +29,5 @@ typedef enum _DRIVE_STATUS
 	DS_INFECTED, // maybe
 	DS_LOCKED
 } DRIVE_STATUS;
-
-#define IOCTL_VOLUME_QUERY_VOLUME_NUMBER        CTL_CODE(IOCTL_VOLUME_BASE, 7, METHOD_BUFFERED, FILE_ANY_ACCESS)
-#define IOCTL_VOLUME_LOGICAL_TO_PHYSICAL        CTL_CODE(IOCTL_VOLUME_BASE, 8, METHOD_BUFFERED, FILE_ANY_ACCESS)
-#define IOCTL_VOLUME_PHYSICAL_TO_LOGICAL        CTL_CODE(IOCTL_VOLUME_BASE, 9, METHOD_BUFFERED, FILE_ANY_ACCESS)
-
-//
-// IOCTL_VOLUME_LOGICAL_TO_PHYSICAL
-//
-// Input Buffer:
-//     Structure of type VOLUME_LOGICAL_OFFSET
-//
-// Output Buffer:
-//     Structure of type VOLUME_PHYSICAL_OFFSETS
-//
-
-typedef struct _VOLUME_LOGICAL_OFFSET {
-
-    //
-    // Specifies the logical location
-    // that needs to be translated to
-    // a physical location.
-    //
-    LONGLONG LogicalOffset;
-
-} VOLUME_LOGICAL_OFFSET, *PVOLUME_LOGICAL_OFFSET;
-
-typedef struct _VOLUME_PHYSICAL_OFFSET {
-
-    //
-    // Specifies the physical location
-    // that needs to be  translated to
-    // a logical location.
-    //
-    ULONG DiskNumber;
-    LONGLONG Offset;
-
-} VOLUME_PHYSICAL_OFFSET, *PVOLUME_PHYSICAL_OFFSET;
-
-typedef struct _VOLUME_PHYSICAL_OFFSETS {
-
-    //
-    // Specifies one or more physical
-    // locations on which the logical
-    // location resides.
-    //
-    ULONG NumberOfPhysicalOffsets;
-    VOLUME_PHYSICAL_OFFSET PhysicalOffset[ANYSIZE_ARRAY];
-
-} VOLUME_PHYSICAL_OFFSETS, *PVOLUME_PHYSICAL_OFFSETS;
 
 #endif // __DRIVEDOTSHIELD_H__
